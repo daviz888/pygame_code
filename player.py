af5c213514c5e54d7a3ce06f7f478ba212282fda
Dopacs <ayos888@gmail.com>
2018-09-17 22:12:54 +0300

    new updates

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 """
 player.py class
 """
+import os
 import pygame
 from pygame.sprite import Sprite
 import random
@@ -10,12 +11,12 @@ class Player(Sprite):
     def __init__(self, ui_settings):
         super().__init__()
         self.ui_settings = ui_settings
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(ui_settings.GREEN)
+        self.image = pygame.image.load(os.path.join(ui_settings.images_path, 'fighter.png')).convert()
+        self.image.set_colorkey(ui_settings.WHITE)
         self.rect = self.image.get_rect()
         self.rect.center = (ui_settings.WIDTH / 2, ui_settings.HEIGHT / 2)
 
     def update(self):
-        self.rect.x += 5
-        if self.rect.left > self.ui_settings.WIDTH:
-            self.rect.right = 0
+        self.rect.top -= 3
+        if self.rect.bottom < 0:
+            self.rect.top = self.ui_settings.HEIGHT
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pygame
 
 class Settings():
@@ -15,5 +16,9 @@ class Settings():
         self.RED = (255, 0, 0)
         self.BLUE = (0, 0, 255)
         self.GREEN= (0, 255, 0)
+
+        # setup asset folders
+        self.game_path = os.path.dirname(__file__)
+        self.images_path = os.path.join(self.game_path,'images')
 
     # def loadImage(self,)
